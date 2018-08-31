d9c09e02fdd357e4a121364c89e409f1098ec80f
mkerbleski <mkerbles@gmail.com>
2018-08-31 10:08:47 -0600

    completed the action route. except for checking

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -22,4 +22,37 @@ router.get('/:id/', (req, res) => {
   })
 });
 
+router.post('/', (req, res) => {
+  //if Id reject db will create automatically
+  //project idRequired  required must be id of existing project
+  //description required only up to 128characters
+  //notes required
+  //complete optional
+  db.insert(req.body).then(newProject  => {
+    res.status(200).json(newProject)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
+});
+
+router.put('/:id/', (req, res) => {
+  //needs id to update else return null
+  //object with changes to apply
+  //reutrns updated object
+  db.update(req.params.id, req.body).then(projects  => {
+    res.status(200).json(projects)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
+});
+
+router.delete('/:id/', (req, res) => {
+  db.remove(req.params.id).then(count  => {
+    res.status(200).json(count)
+  }).catch(err => {
+    res.status(500).json(err)
+  })
+});
+
+
 module.exports = router;
